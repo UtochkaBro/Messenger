9d220881b7b4248e4e896499a468c77edf347d01
Vyacheslav Nikitin <space.face.ru@gmail.com>
2022-10-10 09:47:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/messenger/CreateMessageActivity.java
@@ -0,0 +1,30 @@
+package com.example.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    private EditText editTextMsg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextMsg = findViewById(R.id.editTextMessage);
+    }
+
+    public void onClickSengMsg(View view){
+        String msg = editTextMsg.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, msg);
+        Intent chosenIntent = Intent.createChooser(intent, "Как вы хотите отправить сообщение?");
+        startActivity(chosenIntent);
+
+    }
+}
